5bcbdcf45ac6760254e0003215edfcaf967d6f7f
willprobert <62729041+willprobert@users.noreply.github.com>
2020-03-27 01:07:36 +0000

    Add files via upload

---
+++ tests/test_check.py
@@ -0,0 +1,15 @@
+import subprocess, shutil, os
+from os.path import join
+from string import Template
+import numpy as np, pandas as pd
+import pytest
+
+
+def test_summation():
+    np.testing.assert_equal(0, 0)
+
+def test_summation2():
+    np.testing.assert_equal(2+2, 4)
+
+def test_summation3():
+    np.testing.assert_equal(2+3, 5)
